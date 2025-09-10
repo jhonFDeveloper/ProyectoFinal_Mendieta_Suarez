78d12b2aec9f216c9f3f12211db8bab35990082d
Jhon Fredy <jhonfcompu@gmail.com>
2025-09-10 09:13:56 -0500

    Se agregan iconos y se mejora visuales

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,6 @@
 let cartProducts = JSON.parse(localStorage.getItem("cart") || "[]");
 
-
 const renderProducts = () => {
-
   let container = document.querySelector(".contenedor-productos-carrito");
   let contenidoHTML = "";
 
@@ -12,12 +10,13 @@ const renderProducts = () => {
      <div class="img-producto">
       <img src="${producto.imagen}" alt="${producto.nombre}">
      </div>
-     <div class"info-producto">
-       <h3>$${producto.precio}</h3>
+     <div class="info-producto">
+       <h3>$${producto.precio} x ${producto.cantidad} = $${producto.precio * producto.cantidad}</h3>
        <h5>${producto.nombre}</h5>
+       <p>Cantidad: ${producto.cantidad}</p>
        <button class="btn-eliminar" onclick="removeProduct(${producto.id})">Quitar Producto</button>
      </div>
-        </div>        
+    </div>        
         `;
   });
 
@@ -25,18 +24,26 @@ const renderProducts = () => {
 };
 renderProducts();
 
+let btnEmpty = document.getElementById("vaciar");
 
-let btnEmpty  = document.getElementById("vaciar");
-
-btnEmpty.addEventListener("click" , () => {
-localStorage.removeItem("cart");
-cartProducts = [];
-renderProducts();
+btnEmpty.addEventListener("click", () => {
+  localStorage.removeItem("cart");
+  cartProducts = [];
+  renderProducts();
 });
 
 const removeProduct = (id) => {
-  let arrayPorductsRemove = cartProducts.filter((producto) => producto.id !== id);
-  cartProducts = arrayPorductsRemove;
-  localStorage.setItem("cart", cartProducts);
-  renderProducts();
+  let productoEnCarrito = cartProducts.find((producto) => producto.id === id);
+  
+  if (productoEnCarrito) {
+    if (productoEnCarrito.cantidad > 1) {
+   
+      productoEnCarrito.cantidad -= 1;
+    } else {
+      
+      cartProducts = cartProducts.filter((producto) => producto.id !== id);
+    }
+    localStorage.setItem("cart", JSON.stringify(cartProducts));
+    renderProducts();
+  }
 };
